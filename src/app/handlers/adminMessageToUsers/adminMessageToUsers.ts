aaebe8fc10e867f42cbb25ebce4dc05e7c7c43c9
maziar <maziar.rumiani@gmail.com>
2025-03-22 14:04:33 +0330

    minore change

--- src/app/handlers/adminMessageToUsers/adminMessageToUsers.ts
+++ src/app/handlers/adminMessageToUsers/adminMessageToUsers.ts
@@ -1,16 +1,17 @@
 import { MyContext } from "@/app/bot";
 import { sendMessageToAll } from "./toAll/sendMessageToAll";
 import { toUsersWithNoAssetTrack } from "./toUsersWithNoAssetTrack/toUsersWithNoAssetTrack";
+import { isEmpty } from "lodash";
 
 export async function adminMessageToUsers(ctx: MyContext) {
   const adminId = process.env.TELEGRAM_ADMIN_ID;
   const userId = ctx.from?.id.toString();
   if (!adminId || !userId || userId !== adminId) return;
 
-  const msg = ctx.message!.text!.split("\n").slice(1).join("\n");
+  const msg = ctx.message!.text!.split("\n").slice(1).join("\n").trim();
 
   const [lang, command] = ctx.message!.text!.split("#").slice(1);
-
+  if (isEmpty(msg)) return ctx.reply("Message is empty!");
   // commands handlers
   if (command === "all") {
     return await sendMessageToAll({ ctx, msg, lang });
@@ -18,7 +19,7 @@ export async function adminMessageToUsers(ctx: MyContext) {
     return await toUsersWithNoAssetTrack({ ctx, msg, lang });
   } else {
     ctx.reply(
-      `List of commands by which you can send users a message: #en#all , #fa#all \n #en#usersnoasset , #fa#usersnoasset`
+      `List of commands by which you can send users a message:\n #en#all , #fa#all \n #en#usersnoasset , #fa#usersnoasset`
     );
   }
 }
